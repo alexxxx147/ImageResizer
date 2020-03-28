6dc28412fc95437420f301464f45c34dfa7138ff
alexxxx147 <alexxxx147@gmail.com>
2020-03-29 06:32:54 +0800

    修改為非同步版本

--- ImageResizer/ImageProcess.cs
+++ ImageResizer/ImageProcess.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace ImageResizer
 {
@@ -59,6 +60,41 @@ namespace ImageResizer
         }
 
         /// <summary>
+        /// 進行圖片的縮放作業
+        /// </summary>
+        /// <param name="sourcePath">圖片來源目錄路徑</param>
+        /// <param name="destPath">產生圖片目的目錄路徑</param>
+        /// <param name="scale">縮放比例</param>
+        public async Task ResizeImagesAsync(string sourcePath, string destPath, double scale)
+        {
+            List<Task> Tasks = new List<Task>();
+            var allFiles = FindImages(sourcePath);
+            foreach (var filePath in allFiles)
+            {
+                Tasks.Add(Task.Run(async () =>
+                {
+                    Image imgPhoto = Image.FromFile(filePath);
+                    string imgName = Path.GetFileNameWithoutExtension(filePath);
+
+                    int sourceWidth = imgPhoto.Width;
+                    int sourceHeight = imgPhoto.Height;
+
+                    int destionatonWidth = (int)(sourceWidth * scale);
+                    int destionatonHeight = (int)(sourceHeight * scale);
+
+                    Bitmap processedImage = await processBitmapAsync((Bitmap)imgPhoto,
+                        sourceWidth, sourceHeight,
+                        destionatonWidth, destionatonHeight);
+
+                    string destFile = Path.Combine(destPath, imgName + ".jpg");
+                    processedImage.Save(destFile, ImageFormat.Jpeg);
+                }));
+            }
+            await Task.WhenAll(Tasks);
+        }
+
+
+        /// <summary>
         /// 找出指定目錄下的圖片
         /// </summary>
         /// <param name="srcPath">圖片來源目錄路徑</param>
@@ -93,6 +129,29 @@ namespace ImageResizer
                 new Rectangle(0, 0, srcWidth, srcHeight),
                 GraphicsUnit.Pixel);
             return resizedbitmap;
+        }
+
+        /// <summary>
+        /// 針對指定圖片進行縮放作業
+        /// </summary>
+        /// <param name="img">圖片來源</param>
+        /// <param name="srcWidth">原始寬度</param>
+        /// <param name="srcHeight">原始高度</param>
+        /// <param name="newWidth">新圖片的寬度</param>
+        /// <param name="newHeight">新圖片的高度</param>
+        /// <returns></returns>
+        async Task<Bitmap> processBitmapAsync(Bitmap img, int srcWidth, int srcHeight, int newWidth, int newHeight)
+        {
+            Bitmap resizedbitmap = new Bitmap(newWidth, newHeight);
+            Graphics g = Graphics.FromImage(resizedbitmap);
+            g.InterpolationMode = InterpolationMode.High;
+            g.SmoothingMode = SmoothingMode.HighQuality;
+            g.Clear(Color.Transparent);
+            g.DrawImage(img,
+                new Rectangle(0, 0, newWidth, newHeight),
+                new Rectangle(0, 0, srcWidth, srcHeight),
+                GraphicsUnit.Pixel);
+            return await Task.FromResult(resizedbitmap);
         }
     }
 }
--- ImageResizer/Program.cs
+++ ImageResizer/Program.cs
@@ -9,11 +9,13 @@ namespace ImageResizer
 {
     class Program
     {
-        static void Main(string[] args)
+        async static Task Main(string[] args)
         {
             string sourcePath = Path.Combine(Environment.CurrentDirectory, "images");
             string destinationPath = Path.Combine(Environment.CurrentDirectory, "output"); ;
-
+            long oldProcessTime;
+            long newProcessTime;
+            float improvement;
             ImageProcess imageProcess = new ImageProcess();
 
             imageProcess.Clean(destinationPath);
@@ -22,8 +24,20 @@ namespace ImageResizer
             sw.Start();
             imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
             sw.Stop();
+            Console.WriteLine($"舊花費時間: {sw.ElapsedMilliseconds} ms");
+            oldProcessTime = sw.ElapsedMilliseconds;
 
-            Console.WriteLine($"花費時間: {sw.ElapsedMilliseconds} ms");
+            sw.Reset();
+
+            sw.Start();
+            await imageProcess.ResizeImagesAsync(sourcePath, destinationPath, 2.0);
+            sw.Stop();
+            Console.WriteLine($"新花費時間: {sw.ElapsedMilliseconds} ms");
+
+            newProcessTime = sw.ElapsedMilliseconds;
+            improvement = ((float)(oldProcessTime - newProcessTime) / oldProcessTime)*100;
+            Console.WriteLine($"效能提升: {improvement} %");
+
         }
     }
 }
